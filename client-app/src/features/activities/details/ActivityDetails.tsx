9284624cba705c7196fcc87c84fc0dc099c237e5
Jesperejohannsen <you@example.com>
2023-10-05 10:56:58 +0200

    finishing routing

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,14 +1,23 @@
 import { Card, Image, Button } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { observer } from "mobx-react-lite";
+import { Link, useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 
 const ActivityDetails = () => {
 
   const {activityStore} = useStore();
-  const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+  const {selectedActivity: activity, loadActivity, loadingInitial } = activityStore;
+  const {id} = useParams<{id: string}>();
 
-  if (!activity) return <LoadingComponent />;
+  useEffect(() => {
+    if (id) loadActivity(id);
+
+  }, [id, loadActivity])
+
+  if (loadingInitial || !activity) return <LoadingComponent />;
 
 
   return (
@@ -27,12 +36,12 @@ const ActivityDetails = () => {
       </Card.Content>
       <Card.Content extra>
         <Button.Group widths="2">
-            <Button onClick={() => openForm(activity.id)}basic color="blue" content="Edit" />
-            <Button onClick={cancelSelectedActivity}basic color="grey" content="Close" />
+            <Button as={Link} to={`/manage/${activity.id}`} basic color="blue" content="Edit" />
+            <Button as={Link} to={`/activities`} basic color="grey" content="Close" />
         </Button.Group>
       </Card.Content>
     </Card>
   );
 };
 
-export default ActivityDetails;
+export default observer (ActivityDetails);
